5e9465ea77cf0dd5aafeec1e153afb4e4248abbc
rijul-kansal <kansalrijul@gmail.com>
2024-09-20 14:56:45 +0530

    final testing

---
+++ Utils/keys.js
@@ -0,0 +1,18 @@
+const keys = {
+  type: 'service_account',
+  project_id: 'all-backend-fd5c7',
+  private_key_id: '501300e5fd62d3bd72074915f6cab44f6905fd8f',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCdqy2vwahpjaka\nxR4K1r14Sv0YlgIdt1FyvE8vTlhynkuvHMg15jdM47z6fIdoIJfb5JOQWwe/8Kvq\nz5/r+fMCNbs4+4e3fSjLRfgAB6ENwgV5vWCN7O47Vd7AnW0o7T7wxYJSDttrhOkA\nSx1EiB1vXdjsGeUK5SLjYbdWWSUeK5c24Dc3SakJmwusg9RJc1jvv/hPZASl2qBz\nNVT3g48eECI5SUQKCDZNjT1Of2BTSyW7fjGJCcr9NjnQb9xwe3YbZTtacqxFx8YB\nhlOT8ittgJsNQkkYXxBYSVip13rga7CgqIf9FI0fXkwtiPPBr14VSUOvN0VPOux/\naceKoW5FAgMBAAECggEASxPi+mkl77VEC/pHFvQwXqKeqGFt+oZ7Yz4mGqmc1l9o\nPKdg8sGAHb3oFALOW5gBl/TdC4U9SHmq83IQfcaUn73EPBsSAh64GStlL2Zym2GD\nR/gUjSCARILcQi30d+OY7/+s0bQ/aZBtfvVFp4eFNK2kiLC/7xMzLMEwEw757MYn\nWv2NfVHGoe4tkgQe3dHNSLdXzh1SmWBpbKr0UsptUmdYcIDRv/+wEHeWgAaCbg7y\nGW9PiYCvY2S3slFvTYaVTqUEbJ9NQ+aD1Ws1OK7QiGl8GEJX3WFypqrBIKEGnaEi\n6K+7lhF6lI/O9FghfCcaZOX8RWEsyQ0IEdO4mOFjNQKBgQDVRkmg9/MtMRHTQzyg\nAMC2uUk71aI5NTOM/7IRIsY+qfTvnI+vq3z0AHATz60orVfTCCR6KQgOlA690wWP\nfc7pFSqmiO4qbpU4hLNInHiJkAsePHe0zPQZBYmqBcLCj6r3L/V2TiGvPZd5/lQP\nWe4+iqjwHZi4oFRx+/lrVMuftwKBgQC9QSljlb/D4Z5dRFuQ1X5YVGaSTHtle93P\nWMy70pWHA9a55NRm+UugJUPj3GEsHjMLfjCV0zp0U+xas18MfKEw0Ug7JJ6BG952\nhNjL2fvoLHCm06R5V0LFDXPHYxn30//PuuJ/jTl1enYUfYXPkyc1aVWPbvTv4CE2\njv2oiXWp4wKBgC7KLtkbzMP7YffBZomL4Lu1xrVT1zVJ0Ld8WWqdAHU7ADuZU4Ss\n0MsPVYdjJpAoB36K6MXMPrJEFT/cDfo/FafZNvQ2UloIzMALjPG/MkqryWSmy3Rl\nCqB6sIsWnHXm4xE7+lQsLAOwNY1UGMkNZ9Fcxld3hsvqkNw49a1wewPdAoGAU+gP\n8Zby7mWZUMB92DjPS1ogH7zI+LVyIfu8yYkTwkhxavmJMh5U6SucbenHK0g4WWAZ\nKlP8FpcesxfTjDW1QW+IFrCllZhFiUZc2ARNKyKIz24F/cYyVCsiqcPGP5o+UFsL\nEIIMd4lT0MHWEbGsMIIC7RTEg5VbJFVBIPfQswECgYBVMbroSyB6QhtQ7i2LbjDF\n9h0FGBn34r+gzANF+qddJTU1LRwvOQDhSwprGxpB2ysLIpCJRYQrY8OB2Z5ILySI\nzyTmSxKzR9aAcvNU8h6C1X5ZkL4ws7qcc65p9vSCDx8RZTBbqAmxkQoygdbhEwY/\nuCSoh5jQbUatAaljU5l/Nw==\n-----END PRIVATE KEY-----\n',
+  client_email:
+    'firebase-adminsdk-n3v9l@all-backend-fd5c7.iam.gserviceaccount.com',
+  client_id: '107054637939565406677',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-n3v9l%40all-backend-fd5c7.iam.gserviceaccount.com',
+  universe_domain: 'googleapis.com',
+};
+
+module.exports = { keys };
